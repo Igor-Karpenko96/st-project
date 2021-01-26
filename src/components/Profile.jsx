35618d9b9ac479d4eede1aa83ef052698faf6dac
Igor Karpenko <karpenkoigor961@gmail.com>
2021-01-26 16:14:59 +0200

    start project

---
+++ src/components/Profile.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+function Profile() {
+    return (
+        <div className="content">
+        <div className="content__img">
+          <img
+            src="https://i.pinimg.com/originals/3b/8a/d2/3b8ad2c7b1be2caf24321c852103598a.jpg"
+            alt=""
+          />
+        </div>
+        <div>avatar + description</div>
+        <div>
+          my posts
+          <div>new post</div>
+          <div>
+            <div>post 1</div>
+            <div>post 2</div>
+          </div>
+        </div>
+      </div>
+    )
+}
+
+export default Profile
